46e82eb6d8eb5a2bf5ee8f3752bd547a06c0ef0e
Melodious-nub <suffixit003@gmail.com>
2022-05-19 17:39:07 +0600

    update an delete method will implemented soon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs/internal/observable/from';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'JsonCrudApp';
+  post:any = []
+
+  constructor(private api: ApiService) { }
+  ngOnInit(): void {
+    this.getAllProduct();
+  }
+
+  onSubmit(postForm: any) {
+    if(postForm.valid){
+      this.api.postProduct(postForm.value).subscribe(
+        {next:(res)=>{
+        alert("Prodect Added successfully");
+        postForm.reset();
+      },error:()=>{
+        alert("errror while adding the product")
+      }
+    })
+    }}
+
+    getAllProduct(){
+      this.api.getProduct().subscribe(
+        {next:(res)=>{
+          console.log(res);
+        this.post = res;
+      },error:()=>{
+        alert("errror while fetching the product")
+      }
+    })
+    }
+
 }
